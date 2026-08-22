ad9638db0560cbb617b9487cd6cb108bffa57836
nsalem321 <nadiasalem321@gmail.com>
2026-08-21 21:15:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// ========================================
+// NOVA MASSAGE
+// Basic site scripts
+// ========================================
+
+// Automatically update the copyright year.
+document.getElementById("year").textContent = new Date().getFullYear();
